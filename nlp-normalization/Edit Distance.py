72fd2d3bbf49a6b6feb896bc163f2db6b9b94e32
Matthew <85853890+weezymatt@users.noreply.github.com>
2023-12-15 14:02:55 -0700

    Add update distance

--- nlp-normalization/Edit Distance.py
+++ nlp-normalization/Edit Distance.py
@@ -1,17 +1,19 @@
-# Levenshtein (edit) distance 
-# LC: 72
-# Original algorithm: removal, insertion, or substitution
-# Partially understand. Will need to write the chart
-# down and under the [j - 1][j-1] cases about the pointer
+# Levenshtein distance; LC 72
+# 
+# Reference: 2.5.1 The Minimum Edit Distance Algorithm
+# 
+# Removal, insertion, or substitution; cost = $1 each
+# Figure: 2.17
+
 class Solution:
 	def minDistance(self, word1: str, word2: str) -> int:
 		n = len(word1)
 		m = len(word2)
 
-		# Create a distance matrix D[n+1,m+1];consider base case and reverse!
+		# Create a distance matrix D[n+1,m+1] to consider the null case
 		dp = [[0] * (m+1) for _ in range(n+1)]
 
-		# Initialization: the zeroth row and column is the distance from the empty string
+		# Initialization: zeroth row and column is the distance from the empty string
 		for i in range(1, n + 1):
 			dp[i][0] = i
 
@@ -30,3 +32,6 @@ class Solution:
 					#									DELETE(LEFTWARDS)
 		return dp[n][m]
 
+solver = Solution()
+distance = solver.minDistance("test", "   test")
+print(distance)
