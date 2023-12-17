263631fae64b8b300864574cf64b6ee72c397938
Matthew <85853890+weezymatt@users.noreply.github.com>
2023-12-16 20:34:03 -0700

    Add files via upload

--- perl-scripts/tokenization.pl
+++ perl-scripts/tokenization.pl
@@ -1,13 +1,11 @@
-# Tokenization using Perl
-# simple character-based substitutions
-# accented characters for \n 
+# Tokenization & counts using Perl
+# &
 # use Brown corpus
+# Reference: Language Processing with Perl and Prolog (not my code)
 
 use  utf8;
 binmode(STDOUT, ":encoding(UTF-8)");
 binmode(STDIN, ":encoding(UTF-8)");
-
-
 
 $text = <>;
 while ($line = <>) {
@@ -17,4 +15,16 @@ $text =~ tr/a-zåàâäæçéèêëîïôöœßùûüÿA-ZÅÀÂÄÆÇÉÈÊËÎ
 ’\-,.?!:;/\n/cs;
 $text =~ s/([,.?!:;])/\n$1\n/g;
 $text =~ s/\n+/\n/g;
-print $text;
+# print $text;
+
+@words = split(/\n/, $text);
+for ($i = 0; $i <= $#words; $i++) {
+if (!exists($frequency{$words[$i]})) {
+$frequency{$words[$i]} = 1;
+} else {
+$frequency{$words[$i]}++;
+}
+}
+foreach $word (sort keys %frequency){
+print "$frequency{$word} $word\n";
+}
